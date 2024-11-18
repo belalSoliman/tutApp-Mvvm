404dadcd6cca2cc1b85af5a36a66f747c8659083
belal_soliman <belalsoliman8@gmail.com>
2024-11-18 04:18:27 +0200

    Refactor app structure to use Cupertino widgets and implement singleton pattern for MyApp; add color, font, and value managers; update dependencies and remove unused test file.

---
+++ lib/presentaion/color_manager.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ColorManager {
+  static const Color primaryColor = Color(0xffED9728);
+  static const Color darkGrey = Color(0xff525252);
+  static const Color grey = Color(0xff737477);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color seconderyColor = Color(0xffED9728);
+}
---
+++ lib/presentaion/font_manager.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class FontConstants {
+  static const String fontFamily = 'Montserrat';
+}
+
+class FontWeights {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSizes {
+  static const double small = 12.0;
+  static const double medium = 14.0;
+  static const double large = 16.0;
+  static const double extraLarge = 18.0;
+}
