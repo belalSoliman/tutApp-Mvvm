44e42d1d99db63322f36e663ff749f76360d0e46
belal_soliman <belalsoliman8@gmail.com>
2024-11-20 08:33:56 +0200

    Enhance onboarding view with SVG support and implement page navigation; update asset references and dependencies

--- lib/presentaion/onboarding/on_boarding.dart
+++ lib/presentaion/onboarding/on_boarding.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tutapp/presentaion/resources/assets_manager.dart';
 import 'package:tutapp/presentaion/resources/color_manager.dart';
 import 'package:tutapp/presentaion/resources/strings_manager.dart';
 
@@ -11,24 +13,26 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  final PageController _pageController = PageController();
+  int currentIndex = 0;
   final List<SliderObject> slides = [
     SliderObject(
-      'assets/images/onboarding1.png',
+      ImageAssets.onBoarding1,
       StringsManager.onBoardingTitle1,
       StringsManager.onBoardingDescription1,
     ),
     SliderObject(
-      'assets/images/onboarding2.png',
+      ImageAssets.onBoarding2,
       StringsManager.onBoardingTitle2,
       StringsManager.onBoardingDescription2,
     ),
     SliderObject(
-      'assets/images/onboarding3.png',
+      ImageAssets.onBoarding3,
       StringsManager.onBoardingTitle3,
       StringsManager.onBoardingDescription3,
     ),
     SliderObject(
-      'assets/images/onboarding4.png',
+      ImageAssets.onBoarding4,
       StringsManager.onBoardingTitle4,
       StringsManager.onBoardingDescription4,
     ),
@@ -38,15 +42,46 @@ class _OnBoardingViewState extends State<OnBoardingView> {
     return Scaffold(
       backgroundColor: ColorManager.white,
       appBar: AppBar(
+        backgroundColor: ColorManager.white,
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarColor: ColorManager.white,
-          statusBarBrightness: Brightness.dark,
           statusBarIconBrightness: Brightness.dark,
         ),
+        elevation: 0,
       ),
-      body: Center(
-        child: Text('OnBoarding'),
-      ),
+      body: PageView.builder(
+          controller: _pageController,
+          itemCount: slides.length,
+          onPageChanged: (index) {
+            setState(() {
+              currentIndex = index;
+              print(currentIndex);
+            });
+          },
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Text(
+                  slides[index].title,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  slides[index].description,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: ColorManager.grey,
+                  ),
+                ),
+                SvgPicture.asset(
+                  slides[index].image,
+                  height: 300,
+                ),
+              ],
+            );
+          }),
     );
   }
 }
